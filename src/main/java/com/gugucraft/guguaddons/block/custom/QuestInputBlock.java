ef579440cc0ad1334e89066e3fb8806cadac962e
ThundtrBY <757306685@qq.com>
2026-01-04 17:09:52 +0800

    fix: Add QuestInputBlock class for quest input functionality.

--- src/main/java/com/gugucraft/guguaddons/block/custom/QuestInputBlock.java
+++ src/main/java/com/gugucraft/guguaddons/block/custom/QuestInputBlock.java
@@ -8,16 +8,22 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.Nullable;
 
-public class QuestInputBlock extends DirectionalKineticBlock {
+public class QuestInputBlock extends DirectionalKineticBlock implements EntityBlock {
 
     public QuestInputBlock(Properties properties) {
         super(properties);
+    }
+
+    @Override
+    public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
+        return new QuestInputBlockEntity(pos, state);
     }
 
     public BlockEntityType<? extends KineticBlockEntity> getBlockEntityType() {
