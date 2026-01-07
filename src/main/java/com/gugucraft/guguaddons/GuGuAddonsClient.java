2591c07a48592deead7eab73b7744f3315cbac72
ThundtrBY <757306685@qq.com>
2026-01-07 17:29:28 +0800

    interface connected texture

--- src/main/java/com/gugucraft/guguaddons/GuGuAddonsClient.java
+++ src/main/java/com/gugucraft/guguaddons/GuGuAddonsClient.java
@@ -26,43 +26,64 @@ import com.simibubi.create.foundation.block.connected.CTSpriteShifter;
 @Mod(value = GuGuAddons.MODID, dist = Dist.CLIENT)
 public class GuGuAddonsClient {
 
-    public static final CTSpriteShiftEntry DEDUCTION_CASING_CT = CTSpriteShifter.getCT(
-            AllCTTypes.OMNIDIRECTIONAL,
-            ResourceLocation.fromNamespaceAndPath(GuGuAddons.MODID, "block/deduction_casing"),
-            ResourceLocation.fromNamespaceAndPath(GuGuAddons.MODID, "block/deduction_casing_connected"));
+        public static final CTSpriteShiftEntry DEDUCTION_CASING_CT = CTSpriteShifter.getCT(
+                        AllCTTypes.OMNIDIRECTIONAL,
+                        ResourceLocation.fromNamespaceAndPath(GuGuAddons.MODID, "block/deduction_casing"),
+                        ResourceLocation.fromNamespaceAndPath(GuGuAddons.MODID, "block/deduction_casing_connected"));
 
-    public GuGuAddonsClient(IEventBus modEventBus, ModContainer container) {
-        modEventBus.addListener(GuGuAddonsClient::onClientSetup);
-        modEventBus.addListener(GuGuAddonsClient::registerRenderers);
+        public static final CTSpriteShiftEntry QUEST_INTERFACE_CT = CTSpriteShifter.getCT(
+                        AllCTTypes.OMNIDIRECTIONAL,
+                        ResourceLocation.fromNamespaceAndPath(GuGuAddons.MODID, "block/quest_interface_front"),
+                        ResourceLocation.fromNamespaceAndPath(GuGuAddons.MODID,
+                                        "block/quest_interface_front_connected"));
 
-        // Allows NeoForge to create a config screen for this mod's configs.
-        // The config screen is accessed by going to the Mods screen > clicking on your
-        // mod > clicking on config.
-        // Do not forget to add translations for your config options to the en_us.json
-        // file.
-        container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
-    }
+        public GuGuAddonsClient(IEventBus modEventBus, ModContainer container) {
+                modEventBus.addListener(GuGuAddonsClient::onClientSetup);
+                modEventBus.addListener(GuGuAddonsClient::registerRenderers);
 
-    static void onClientSetup(FMLClientSetupEvent event) {
-        // Client setup code
-        GuGuAddons.LOGGER.info("HELLO FROM CLIENT SETUP");
-        GuGuAddons.LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
+                // Allows NeoForge to create a config screen for this mod's configs.
+                // The config screen is accessed by going to the Mods screen > clicking on your
+                // mod > clicking on config.
+                // Do not forget to add translations for your config options to the en_us.json
+                // file.
+                container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
+        }
 
-        dev.engine_room.flywheel.lib.visualization.SimpleBlockEntityVisualizer
-                .builder(ModBlockEntities.QUEST_INPUT.get())
-                .factory(OrientedRotatingVisual.of(AllPartialModels.SHAFT_HALF))
-                .skipVanillaRender(be -> true)
-                .apply();
+        static void onClientSetup(FMLClientSetupEvent event) {
+                // Client setup code
+                GuGuAddons.LOGGER.info("HELLO FROM CLIENT SETUP");
+                GuGuAddons.LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
 
-        // Register Connected Textures
-        CreateClient.MODEL_SWAPPER.getCustomBlockModels().register(ModBlocks.DEDUCTION_CASING.getId(),
-                model -> new CTModel(model, new EncasedCTBehaviour(DEDUCTION_CASING_CT)));
-        CreateClient.CASING_CONNECTIVITY.makeCasing(ModBlocks.DEDUCTION_CASING.get(), DEDUCTION_CASING_CT);
-    }
+                dev.engine_room.flywheel.lib.visualization.SimpleBlockEntityVisualizer
+                                .builder(ModBlockEntities.QUEST_INPUT.get())
+                                .factory(OrientedRotatingVisual.of(AllPartialModels.SHAFT_HALF))
+                                .skipVanillaRender(be -> true)
+                                .apply();
 
-    public static void registerRenderers(
-            net.neoforged.neoforge.client.event.EntityRenderersEvent.RegisterRenderers event) {
-        event.registerBlockEntityRenderer(ModBlockEntities.QUEST_INPUT.get(),
-                com.gugucraft.guguaddons.client.renderer.QuestInputRenderer::new);
-    }
+                // Register Connected Textures
+                CreateClient.MODEL_SWAPPER.getCustomBlockModels().register(ModBlocks.DEDUCTION_CASING.getId(),
+                                model -> new CTModel(model, new EncasedCTBehaviour(DEDUCTION_CASING_CT)));
+                CreateClient.CASING_CONNECTIVITY.makeCasing(ModBlocks.DEDUCTION_CASING.get(), DEDUCTION_CASING_CT);
+
+                CreateClient.MODEL_SWAPPER.getCustomBlockModels().register(ModBlocks.QUEST_INTERFACE_BLOCK.getId(),
+                                model -> new CTModel(model, new EncasedCTBehaviour(DEDUCTION_CASING_CT) {
+                                        @Override
+                                        public CTSpriteShiftEntry getShift(
+                                                        net.minecraft.world.level.block.state.BlockState state,
+                                                        net.minecraft.core.Direction direction,
+                                                        @org.jetbrains.annotations.Nullable net.minecraft.client.renderer.texture.TextureAtlasSprite sprite) {
+                                                return direction == state.getValue(
+                                                                com.gugucraft.guguaddons.block.custom.QuestInterfaceBlock.FACING)
+                                                                                ? QUEST_INTERFACE_CT
+                                                                                : DEDUCTION_CASING_CT;
+                                        }
+                                }));
+                CreateClient.CASING_CONNECTIVITY.makeCasing(ModBlocks.QUEST_INTERFACE_BLOCK.get(), DEDUCTION_CASING_CT);
+        }
+
+        public static void registerRenderers(
+                        net.neoforged.neoforge.client.event.EntityRenderersEvent.RegisterRenderers event) {
+                event.registerBlockEntityRenderer(ModBlockEntities.QUEST_INPUT.get(),
+                                com.gugucraft.guguaddons.client.renderer.QuestInputRenderer::new);
+        }
 }
